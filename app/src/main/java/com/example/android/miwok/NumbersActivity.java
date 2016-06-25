b8473004decbf9081852830d422024a94e083d2e
Luis Perez <lperez.1092@gmail.com>
2016-06-25 15:50:06 -0430

    Crea TextViews de los elementos en la lista de palabras (números) y los imprime sobre el XML de la categoría Numbers.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,5 +25,14 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Eight");
         words.add("Nine");
         words.add("Ten");
+
+        // Finds activity's root view
+        LinearLayout numbersRootView = (LinearLayout) findViewById(R.id.numbers_root_view);
+        // Creates TextViews in a loop to populate previously found ViewGroup
+        for (int i = 0; i < words.size(); i++){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            numbersRootView.addView(wordView);
+        }
     }
 }
