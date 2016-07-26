26bf56f06af46a5956bf39f38396ed85ae1e53b6
Luis Perez <lperez.1092@gmail.com>
2016-07-25 23:19:37 -0430

    Agrega TabLayout al ViewPager y lo estiliza

--- app/src/main/java/com/example/android/miwok/CategoryPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryPagerAdapter.java
@@ -1,14 +1,18 @@
 package com.example.android.miwok;
 
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryPagerAdapter extends FragmentPagerAdapter {
 
-    public CategoryPagerAdapter(FragmentManager fm) {
+    private Context mContext;
+
+    public CategoryPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -27,5 +31,18 @@ public class CategoryPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
