38b50d5b4a769eb2ec0b2fd58fae55172cdd5797
Luis Pérez <lperez.1092gmail.com>
2017-05-06 01:32:52 -0400

    Utiliza string resources para vocabulario por defecto

--- app/src/main/java/com/example/android/miwok/NumbersFragment.java
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -76,16 +76,16 @@ public class NumbersFragment extends Fragment {
 
         // Create list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
-        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
-        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
-        words.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
-        words.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
+        words.add(new Word(getString(R.string.number_one), "lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word(getString(R.string.number_two), "otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word(getString(R.string.number_three), "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word(getString(R.string.number_four), "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word(getString(R.string.number_five), "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word(getString(R.string.number_six), "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word(getString(R.string.number_seven), "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word(getString(R.string.number_eight), "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word(getString(R.string.number_nine), "wo’e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word(getString(R.string.number_ten), "na’aacha", R.drawable.number_ten, R.raw.number_ten));
 
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
